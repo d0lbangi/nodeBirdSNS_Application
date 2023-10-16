d158a9e6ab945c58ad6d3e929a8b2e21d2714ee2
Jeongho Park <d0lbangi13@naver.com>
2023-10-16 16:26:46 +0900

    FEAT: Add page for controllers

--- README.md
+++ README.md
@@ -9,3 +9,5 @@ npm i -D nodemon
 npx sequelize db:create // config.json을 읽어 데이터베이스를 생성함
 
 npm i passport passport-local passport-kakao bcrypt // Passport 관련 패키지 설치
+
+npm i multer // 이미지 업로드 구현하기
--- routes/auth.js
+++ routes/auth.js
@@ -2,28 +2,27 @@ const express = require('express');
 const passport = require('passport');
 
 const { isLoggedIn, isNotLoggedIn } = require('../middlewares');
-const { join, login, logout } =require('../controllers/auth');
+const { join, login, logout } = require('../controllers/auth');
 
-const router = express.router();
+const router = express.Router();
 
 // POST /auth/join
-router.post('join', isNotLoggedIn, join);
+router.post('/join', isNotLoggedIn, join); 
 
 // POST /auth/login
-router.post('login', isNotLoggedIn, login);
+router.post('/login', isNotLoggedIn, login);
 
-// POST /auth/logout
-router.post('logout', isNotLoggedIn, logout);
+// GET /auth/logout
+router.get('/logout', isLoggedIn, logout);
 
-// kakao login router
 // GET /auth/kakao
 router.get('/kakao', passport.authenticate('kakao'));
 
 // GET /auth/kakao/callback
 router.get('/kakao/callback', passport.authenticate('kakao', {
-  failureRedirect: '/?loginError=카카오로그인 실패',
+  failureRedirect: '/?error=카카오로그인 실패',
 }), (req, res) => {
-  res.redirect('/'); 
+  res.redirect('/'); // 성공 시에는 /로 이동
 });
 
 module.exports = router;
